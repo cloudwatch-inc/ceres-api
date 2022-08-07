c819576becb2e4d9db1f6562f159369c2ed735a2
Mark Andog <andogreymark@gmail.com>
2022-08-07 21:46:22 +0800

    feat: prisma user seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,39 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import { user } from './seeds/user.seed';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const user1 = await prisma.user.upsert({
-    where: { username: 'admin' },
-    update: {},
-    create: {
-      username: 'admin',
-      email: 'admin@ceres.com',
-      firstName: 'Admin',
-      lastName: 'Admin',
-      password: 'Hello101!',
-      isActive: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  });
-
-  const user2 = await prisma.user.upsert({
-    where: { username: 'user' },
-    update: {},
-    create: {
-      username: 'user',
-      email: 'user@ceres.com',
-      firstName: 'User',
-      lastName: 'User',
-      password: 'Hello101!',
-      isActive: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  });
-
-  console.log({ user1, user2 });
+  await user();
 }
 
 main()
